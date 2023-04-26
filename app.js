ff2299daaf024063de0e144ca1d3484aeae1bb62
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-04-26 15:25:41 -0400

    implemented authentication

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session');
+const passport = require('passport');
+require('./config/passport')(passport);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -14,7 +20,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+    session({
+        secret: 'your_secret_key',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
