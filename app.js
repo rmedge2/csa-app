0a03dc77ff3a94e085f461f28818c78ae410c525
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-04-27 14:18:01 -0400

    added all routes for baskets and included authentication

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var usersRouter = require('./routes/users');
 
 const authRouter = require('./routes/auth');
 
+const basketsRouter = require('./routes/baskets');
+const itemsRouter = require("./routes/items");
+const ordersRouter = require("./routes/orders");
+
 var app = express();
 
 app.use(logger('dev'));
@@ -33,5 +37,8 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+app.use("/baskets", basketsRouter);
+// app.use("/items", itemsRouter);
+// app.use("/orders", ordersRouter);
 
 module.exports = app;
--- routes/baskets.js
+++ routes/baskets.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { authenticate } = require("../middlewares/auth");
+
 
 const { Basket, BasketItem, Item } = require('../models');
 
 //Creating a new basket
-router.post('/', async (req, res) => {
+router.post('/', authenticate, authenticate, async (req, res) => {
+    const { name, price } = req.body;
     try {
-        const basket = await Basket.create(req.body);
+        const basket = await Basket.create({ name, price });
         res.status(201).json(basket)
     } catch (error) {
         res.status(500).json({ message: 'Error creating the basket', error });
@@ -18,20 +21,20 @@ router.get('/', async (req, res) => {
     try {
         const baskets = await Basket.findAll();
         res.json(baskets);
-    } catch (error){
+    } catch (error) {
         res.status(500).json({ message: 'Error retrieving baskets', error });
     }
 });
 
 // Get a specific basket by ID, including associated items
-router.get('/:id', async (req,res) => {
+router.get('/:id', async (req, res) => {
     try {
         const basket = await Basket.findByPk(req.params.id);
 
-        if (!basket){
+        if (!basket) {
             res.status(404).json({ message: 'Basket not found' });
         } else {
-            res.status(404).json({ message: 'Basket not found' });
+            res.json(basket);
         }
     } catch (error) {
         res.status(500).json({ message: 'Error updating basket', error });
@@ -39,12 +42,21 @@ router.get('/:id', async (req,res) => {
 });
 
 // Update a basket by ID
-router.put('/:id', async (req,res) => {
+router.put('/:id', authenticate, async (req, res) => {
+    const { name, price } = req.body;
+    
     try {
+        const newBasket = {};
+        if (name !== undefined) {
+            newBasket.name = name;
+        }
+        if (price !== undefined) {
+            newBasket.price = price;
+        }
         const [updated] = await Basket.update(req.body, {
             where: { id: req.params.id },
         });
-        if (updated){
+        if (updated) {
             const updatedBasket = await Basket.findByPk(req.params.id);
             res.json(updatedBasket);
         } else {
@@ -56,6 +68,19 @@ router.put('/:id', async (req,res) => {
 });
 
 // Delete a basket by ID
-router.delete('/:id')
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleted = await Basket.destroy({
+            where: { id: req.params.id },
+        });
+        if (deleted) {
+            res.status(204).json({ message: "Basket deleted" });
+        } else {
+            res.status(404).json({ message: "Basket not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting basket" });
+    }
+});
 
 module.exports = router;
